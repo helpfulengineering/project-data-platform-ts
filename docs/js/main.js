27232202576f5cd3395db54b6da05fd3674b1843
Robert L. Read <read.robert@gmail.com>
2024-03-26 18:26:21 -0500

    trying to add table

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,33 @@
+use strict;
+
+OKHS = [];
+OKWS = [];
+
+OKHS[0] = { name: "first OKH" };
+
+OKWS[0] = { name: "first OKW" };
+
+function registerOKH(name,number,desc) {
+  var table = document.getElementById("okh_table");
+
+  var row = table.insertRow(-1);
+
+  row.id = number;
+  row.setAttribute("class_num", class_num);
+  row.setAttribute("solid_name", name);
+
+  var cnt = 0;
+  var name_c = row.insertCell(cnt++);
+    var desc_c = row.insertCell(cnt++);
+var button_c = row.insertCell(cnt++);
+
+  button_c.innerHTML = "<button onclick='renderOKH(\""+
+    number")'>Go</button>";
+}
+
+
 function runUnitTests() {
     alert("no tests yet!");
 }
+
+$(registerOKH(OKH[0]));
