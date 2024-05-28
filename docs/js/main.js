c552659180f5c68dbd1a824e3454119d47c4eddf
Robert L. Read <read.robert@gmail.com>
2024-05-28 18:19:11 -0500

    some experiments tried but not working

--- docs/js/main.js
+++ docs/js/main.js
@@ -154,6 +154,10 @@ function xml2json(xml, tab) {
    return "{\n" + tab + (tab ? json.replace(/\t/g, tab) : json.replace(/\t|\n/g, "")) + "\n}";
 }
 
+
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+
 // Possibly this is a better thing to try:
 // https://github.com/abdolence/x2js
 
@@ -184,51 +188,9 @@ var albumBucketName = "github-helpfulengineering-library";
 
 example_id = '8fb8567d3a69838c469b205b7cf70b8c1abeddf0cdc9fd63f367006b6fc78262';
 
+
+
 function robTriesToUseHarrysClientSideS3() {
-    // Initialize the Amazon Cognito credentials provider
-    // AWS.config.region = 'YOUR_REGION'; // e.g., us-east-1
-
-    // This worked...
-    var url = `https://s3.amazonaws.com/${albumBucketName}`;
-    fetch(url
-//           ,
-//           {
-//               method: "GET", // *GET, POST, PUT, DELETE, etc.
-//               mode: "no-cors", // no-cors, *cors, same-origin
-//     // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     // credentials: "same-origin", // include, *same-origin, omit
-//               headers: {
-// //                  "Content-Type": "application/json",
-//                   "Accept": "application/json"
-//               }}
-         )
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text();
-  })
-  .then(data => {
-      console.log(data); // Do something with the content of the file
-
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(data, "application/xml");
-      console.log(doc);
-
-      try {
-      const json =  xml2json(doc);
-          console.log(json);
-          objects = JSON.parse(json);
-          console.log(objects);
-          debugger;
-      } catch(error) {
-          console.log("xml2json failed");
-          console.log(error);
-      };
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
 
     AWS.config.credentials = new AWS.CognitoIdentityCredentials(
         {
@@ -237,42 +199,42 @@ function robTriesToUseHarrysClientSideS3() {
         }
     );
 
-//     // Create S3 service object
-//     var s3 = new AWS.S3();
+    // Create S3 service object
+    var s3 = new AWS.S3();
 
-//     const params = {
-//         Bucket: albumBucketName,
-//         Key: 'alpha/okh/okh-Character-Generator.yml'
-//     };
+    const params = {
+        Bucket: albumBucketName,
+        Key: 'alpha/okh/okh-Character-Generator.yml'
+    };
 
-//     s3.getObject(params, (err, data) => {
-//   if (err) {
-//     console.error('Error retrieving object:', err);
-//   } else {
-//     console.log('Object data:', data.Body.toString());
-//   }
-// });
+    s3.getObject(params, (err, data) => {
+  if (err) {
+    console.error('Error retrieving object:', err);
+  } else {
+    console.log('Object data:', data.Body.toString());
+  }
+});
 
-//     // Call S3 to list the buckets
-//     s3.listObjects({ Bucket: albumBucketName }, function (err, data) {
-//         if (err) {
-//             console.log('Error', err);
-//         } else {
-//             console.log('Success', data);
-//         }
-//     });
+    // Call S3 to list the buckets
+    s3.listObjects({ Bucket: albumBucketName }, function (err, data) {
+        if (err) {
+            console.log('Error', err);
+        } else {
+            console.log('Success', data);
+        }
+    });
 
 
-    // region = "us-east-2";
-    // releaseName = "debug-proxy";
-    // //    regexVersion = new RegExp(`^${this.releaseName}\/(?<Version>.*)\/$`);
+    region = "us-east-2";
+    releaseName = "debug-proxy";
+    //    regexVersion = new RegExp(`^${this.releaseName}\/(?<Version>.*)\/$`);
 
-    // // Note: Using an identity signer function is a workaround for an AWS SDk issue
-    // // https://github.com/aws/aws-sdk-js-v3/issues/2321#issuecomment-916336230
-    // var s3 = S3Client({
-    //   region: region,
-    //   signer: { sign: (request) => Promise.resolve(request) }
-    // });
+    // Note: Using an identity signer function is a workaround for an AWS SDk issue
+    // https://github.com/aws/aws-sdk-js-v3/issues/2321#issuecomment-916336230
+    var s3 = S3Client({
+      region: region,
+      signer: { sign: (request) => Promise.resolve(request) }
+    });
 }
 
 
@@ -316,7 +278,128 @@ OKWS = [];
 
 OKHS[0] = { name: "first OKH" };
 
-OKWS[0] = { name: "first OKW" };
+VENTMON_EXAMPLE_WIRED_OKH = {
+    title: "VentMon v0.4T",
+description: "The VentMon is basically a cloud-enable spirometer with an FiO2 sensor. It can\
+  simply be plugged into a standard 22mm airway and provides a rich stream of\
+  pressure, flow, and Oxygen sensor, with humidity and temperature sensing as\
+  well. The primary use of the VentMon is by engineering teams developing\
+  pandemic response ventilators and other respiration tools. Secondarily, it is\
+  completely and transparently open source, so any team can adopt the design and\
+  incorporate it into their own devices to provide the same data in an\
+  integrated way.",
+    intended_use: "The original purpose of the VentMon was to support pandemic response teams\
+  creating emergency ventilators. Interposed between a ventilator under test and\
+  a plastic test lung, it provides a display similar to the clinical display on\
+  modern ventilators, allow engineers to validate all mechanical function of the\
+  ventilators. Additionally, it compute rise time as a performance metric. It\
+  produces data in the PIRDS standard, which can be logged and is later\
+  processed by the VentDisplay software.",
+keywords: [
+    "ventilation",
+    "spirometry",
+    "pandemic response",
+    "arduino",
+    "open source",
+    "free software"],
+    project_link: "https://github.com/PubInv/ventmon-ventilator-inline-test-monitor",
+    image: "https://user-images.githubusercontent.com/5296671/113649478-1e9da800-9654-11eb-934b-7e8a9a5c5bfe.JPG",
+    made: "true",
+    made_independently: "false",
+    license: {
+        hardware: "CERN-OHL-1.2",
+        documentation: "CC0-1.0",
+        software: "GPL-3.0-only"},
+    licensor: {
+        name: "Robert L. Read",
+        affiliation: "Public Invention (https://www.pubinv.org/)",
+        email: "read.robert@gmail.com"
+    },
+    okh_manifest_version: "1.0.0",
+    date_created: "2021-06-01",
+    date_updated: "2021-06-01",
+    manifest_author: {
+        name: "Robert L. Read",
+        affiliation: "Public Invention (https://www.pubinv.org/)",
+        email: "read.robert@gmail.com"
+    },
+// manifest-language: en-US
+// contact:
+//   name: Robert L. Read
+//   affiliation: Public Invention (https://www.pubinv.org/)
+//   email: read.robert@gmail.com
+// contributors:
+//   - name: Lauria Clarke
+//     affiliation: Public Invention
+//   - name: Geoff Mulligan
+//     affiliation: Public Invention
+//   - name: Ben Coombs
+//     affiliation: Public Invention
+// version: v0.3T and V0.4T
+// development-stage: product
+// documentation-home: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor
+// archive-download: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor/releases/tag/v0.3.1T
+// schematics:
+//   - path: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor/blob/master/design/pcb/ventmon_T0.4/ventmon_t0.42.sch
+//     title: T0.4 PCB schmatics (look for complete Eagle files)
+// bom: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor/blob/master/design/pcb/ventmon_T0.4/VentMon%20T0.4%20BOM.csv
+// making-instructions:
+//   - path: https://www.sciencedirect.com/science/article/pii/S2468067221000249
+//     title: "VentMon: An open source inline ventilator tester and monitor"
+//   - path: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor
+//     title: Repo for the VentMon
+// manufacturing-files:
+//   - path: https://github.com/PubInv/ventmon-ventilator-inline-test-monitor/tree/master/design/3dparts
+//     title: 3d printed files can be found here
+// operating-instructions:
+//   - path: https://github.com/PubInv/vent-display
+//     title: VentDisplay explanations, including useful video
+// software:
+//   - path: https://github.com/PubInv/vent-display
+//     title: VentDisplay
+//   - path: https://github.com/PubInv/PIRDS-pubinv-respiration-data-standard
+//     title: Public Invention Respiration Data Standard
+//   - path: https://github.com/PubInv/PIRDS-logger
+//     title: PIRDS logger
+//   - path: https://github.com/PubInv/PIRDS-docker-local
+//     title: PIRDS docker instance
+// health-safety-notice: This is not intended to be used on live patients.
+// standards-used:
+//   - standard-title: https://github.com/PubInv/PIRDS-pubinv-respiration-data-standard
+//                                  publisher: Public Invention
+                                };
+
+    OKWS[0] = { name: "first OKW" };
+
+async function harrysS3Approach() {
+    const s3Client = new S3Client({
+        region: "us-east-1",
+        // https://github.com/aws/aws-sdk-js-v3/issues/2321#issuecomment-916336230
+        signer: { sign: (request) => Promise.resolve(request) }
+    })
+    const bucketName = "github-helpfulengineering-library"
+
+    try {
+        const paginator = paginateListObjectsV2(
+            { client: s3Client },
+            { Bucket: bucketName }
+        );
+
+        for await (const page of paginator) {
+            const objects = page.Contents ?? [];
+            for (const object of objects) {
+                console.log(object.Key);
+                // const response = await s3Client.send(new GetObjectCommand({ Bucket: bucketName, Key: object.Key }));
+                // const body = await response.Body?.transformToString();
+                // const qq = YAML.parse(body ?? "");
+            }
+        }
+
+
+    } finally {
+        s3Client.destroy();
+    }
+}
 
 function registerOKH(okh) {
 
@@ -350,7 +433,55 @@ function runUnitTests() {
 
 function init() {
     registerOKH(OKHS[0]);
-    robTriesToUseHarrysClientSideS3();
+    harrysS3Approach();
+//    robTriesToUseHarrysClientSideS3();
  //   listAlbums();
+}
+
+function fetchFromS3ViaURL() {
+        // Initialize the Amazon Cognito credentials provider
+    // AWS.config.region = 'YOUR_REGION'; // e.g., us-east-1
+
+    // This worked...
+    var url = `https://s3.amazonaws.com/${albumBucketName}`;
+    fetch(url
+//           ,
+//           {
+//               method: "GET", // *GET, POST, PUT, DELETE, etc.
+//               mode: "no-cors", // no-cors, *cors, same-origin
+//     // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     // credentials: "same-origin", // include, *same-origin, omit
+//               headers: {
+// //                  "Content-Type": "application/json",
+//                   "Accept": "application/json"
+//               }}
+         )
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+      console.log(data); // Do something with the content of the file
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, "application/xml");
+      console.log(doc);
+
+      try {
+      const json =  xml2json(doc);
+          console.log(json);
+          objects = JSON.parse(json);
+          console.log(objects);
+          debugger;
+      } catch(error) {
+          console.log("xml2json failed");
+          console.log(error);
+      };
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 }
 $(init());
