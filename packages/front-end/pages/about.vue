678acf3efd9cee4031ea00a6691b7086d595dd22
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-24 18:08:03 -0500

    pages added

---
+++ packages/front-end/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1 class="about-us text-2xl font-bold tracking-tight text-red-900">
+      About Us
+    </h1>
+  </section>
+</template>
+
+<style>
+.about-us {
+  margin: 150px;
+}
+</style>
---
+++ packages/front-end/pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1 class="register-text text-2xl font-bold tracking-tight text-red-900">
+      Register
+    </h1>
+  </section>
+</template>
+
+<style>
+.register-text {
+  margin: 150px;
+}
+</style>
