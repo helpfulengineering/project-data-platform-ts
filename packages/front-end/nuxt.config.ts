bc4af654b66550186bf1a93f2f94786847fd826b
Robert L. Read <read.robert@gmail.com>
2024-09-27 00:43:43 -0500

    end of day --- tie to back end working, but data not meaningful

--- packages/front-end/nuxt.config.ts
+++ packages/front-end/nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// WARNING!!!
+// VOODOO ALERT!!!
+// Changing from "localhost" to 127.0.0.1
+// made a huge difference here.
+// This is totally unexplained and complete nonsense, but true.
+const globalBaseUrl = 'http://127.0.0.1:7071/api';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+    compatibilityDate: '2024-04-03',
+    // routeRules: {
+    //     '/api/**': {
+    //         proxy: { to: globalBaseUrl + "/**" },
+    //      },
+    // },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
+     runtimeConfig: {
+        public: {
+            baseUrl: process.env.BACKEND_URL ? process.env.BACKEND_URL : globalBaseUrl,
+//            apiBase: '/api'
+        }
+    },
+
   postcss: {
     plugins: {
       tailwindcss: {},
