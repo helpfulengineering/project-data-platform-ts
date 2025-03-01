ac4df6530090fe78ba40f7a3f0ce67ef3a3ff9a4
NimeshaKahingala <nimesha.isholi94@gmail.com>
2025-03-01 17:50:31 -0600

    lazy loading and css improvements

--- packages/front-end/pages/products/[id].vue
+++ packages/front-end/pages/products/[id].vue
@@ -1,86 +1,129 @@
+<script setup lang="ts">
+import { useRoute } from "#app";
+import type { OKH_TYPE } from "../../types/OKH.type";
+import {ref, computed } from 'vue';
+import { formatKeywords, formatImages } from "~/utils/utils";
+
+
+const route = useRoute();
+
+const baseUrl = useRuntimeConfig().public.baseUrl;
+
+const productFilename = route.params.id as string;
+const [fname, fileExt] = productFilename.split(".");
+
+const url = baseUrl + "/getFile/okh/" + fname + "/" + fileExt;
+
+// const url = baseUrl + "/getFile/okh/" + productFilename
+//const url = "http://demo4460398.mockable.io/details";
+
+
+const { data, status, error } = await useFetch(url, {
+  lazy: true,
+});
+
+const sliderImages = ref<string[]>([]);
+const product = ref<any>({});
+
+watchEffect(() => {
+  product.value = data.value?.product;
+  sliderImages.value = formatImages(data.value?.product?.image);
+});
+console.log("sliderImages",sliderImages)
+// const { data, status, error, refresh   } = useAsyncData(url, () =>
+//   $fetch(url)
+// );
+</script>
+
 <template>
-  <div>
+  <div class="product-detail-page">
+    <div v-if="status === 'error'" class="error">
+      error : {{ error?.message }}
+    </div>
     <div
       v-if="status === 'pending'"
-      class="loading skelton-card-group"
-      style="color: red"
+      class="loader skelton-card-group"
     >
-      loading
+    </div>
+    <div v-else-if="product === null" class="no-product">
+      <p>Product Not found.</p>
+      <p>{{ url }}</p>
     </div>
     <div
       v-else-if="status === 'success'"
       class="product-detail container center m-10"
     >
       <div class="left">
-        <!-- <Slider :images="data.product?.image" /> -->
+        <Slider v-if="sliderImages.length > 0" :images="sliderImages"/>
 
         <div class="specification">Specifications</div>
         <div class="okh-details">
           <div>version</div>
-          <div class="value">{{ data.product?.version }}</div>
+          <div class="value">{{product?.version }}</div>
         </div>
         <div class="okh-details">
           <div>License</div>
-          <div class="value">{{ data.product?.license }}</div>
+          <div class="value">{{product?.license }}</div>
         </div>
         <div class="okh-details">
           <div>Licensor</div>
           <div class="value">
-            <div>{{ data.product?.licensor.name }}</div>
-            <div>{{ data.product?.licensor.email }}</div>
-            <div>{{ data.product?.licensor.affiliation }}</div>
+            <div>{{ product?.licensor?.name }}</div>
+            <div>{{ product?.licensor?.email }}</div>
+            <div>{{ product?.licensor?.affiliation }}</div>
           </div>
         </div>
         <div class="okh-details">
           <div>manifest author</div>
           <div class="value">
-            <div>{{ data.product["manifest-author"].name }}</div>
-            <div>{{ data.product["manifest-author"].email }}</div>
-            <div>{{ data.product["manifest-author"].affiliation }}</div>
+            <div>{{ product["manifest-author"]?.name }}</div>
+            <div>{{ product["manifest-author"]?.email }}</div>
+            <div>{{ product["manifest-author"]?.affiliation }}</div>
           </div>
         </div>
         <div class="okh-details">
           <div>manifest language</div>
-          <div class="value">{{ data.product["manifest-language"] }}</div>
+          <div class="value">{{ product["manifest-language"] }}</div>
         </div>
         <div class="okh-details">
           <div>okh manifest version</div>
-          <div class="value">{{ data.product["okh-manifest-version"] }}</div>
+          <div class="value">{{ product["okh-manifest-version"] }}</div>
         </div>
         <div class="okh-details">
           <div>date created</div>
-          <div class="value">{{ data.product["date-created"] }}</div>
+          <div class="value">{{ product["date-created"] }}</div>
         </div>
         <div class="okh-details">
           <div>date updated</div>
-          <div class="value">{{ data.product["date-updated"] }}</div>
+          <div class="value">{{ product["date-updated"] }}</div>
         </div>
         <div class="okh-details">
           <div>keywords</div>
-          <div class="value">{{ data.product?.keywords?.join(", ") }}</div>
+          <div class="value">{{ formatKeywords(product?.keywords) }}</div>
+          {{ console.log('Keywords:', formatKeywords(product?.keywords)) }}
         </div>
         <div class="okh-details">
           <div>contact</div>
           <div class="value">
-            <div>{{ data.product.contact.name }}</div>
-            <div>{{ data.product.contact.email }}</div>
-            <div>{{ data.product.contact.affiliation }}</div>
+            <div>{{ product?.contact?.name }}</div>
+            <div>{{ product?.contact?.email }}</div>
+            <div>{{ product?.contact?.affiliation }}</div>
           </div>
         </div>
         <div class="okh-details">
           <div>development stage</div>
-          <div class="value">{{ data.product["development-stage"] }}</div>
+          <div class="value">{{ product["development-stage"] }}</div>
         </div>
         <div class="okh-details">
           <div>health safety notice</div>
-          <div class="value">{{ data.product["health-safety-notice"] }}</div>
+          <div class="value">{{ product["health-safety-notice"] }}</div>
         </div>
       </div>
       <div class="center">
-        <h1 class="title">{{ data.product?.title }}</h1>
+        <h1 class="title">{{ product?.title }}</h1>
 
         <div class="location">Location, Country</div>
-        <p>{{ data.product?.description }}</p>
+        <p>{{ product?.description }}</p>
         <div class="review-wrap">
           <Reviews />
           <Reviews />
@@ -93,51 +136,34 @@
       </div>
       <!-- <NuxtLink to="/">Back to List</NuxtLink> -->
     </div>
-
-    <div v-if="status === 'error'">error : {{ error?.message }}</div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { useRoute } from "#app";
-import type { OKH_TYPE } from "../../types/OKH.type";
-
-const route = useRoute();
-
-// Apparently This is being called with a large number of values which
-// are incorrect. I do not know why. I suppose this might have to
-// do with the idea of "eager fetching". I am going to attempt
-// to at least have this identify this and fail!
-
-// changing this to a filename..
-const productFilename = route.params.id as string;
-
-console.log("productFilename", productFilename);
-
-const productId = route.params.id as string;
-
-const baseUrl = useRuntimeConfig().public.baseUrl;
-
-const [fname, fileExt] = productFilename.split(".");
-
-const url = baseUrl + "/getFile/okh/" + fname + "/" + fileExt;
-
-// const url = baseUrl + "/getFile/okh/" + productFilename
-//const url = "http://demo4460398.mockable.io/details";
-console.log("url", url);
-const { data, status, error } = await useFetch(url);
-
-console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-console.log(data?.product);
-
-</script>
-
 <style scoped>
+.product-detail-page {
+  margin: 130px auto 0 auto;
+
+  .loading {
+    font-size: 30px;
+    color: red;
+    background-color: green;
+    margin: 100px 0;
+    padding: 10px;
+    z-index: 10000;
+  }
+
+  .error {
+    margin: 100px 0;
+    color: red;
+    font-size: 20px;
+  }
+}
+
 .product-detail {
   background-color: white;
   display: flex;
   justify-content: space-between;
-  margin: 130px auto 40px auto;
+  margin: 0px auto 40px auto;
   padding: 30px;
 
   .btn-primary {
@@ -214,12 +240,30 @@ console.log(data?.product);
     font-size: 24px;
     font-weight: 700;
   }
+
+  
 }
 
-.loading {
-  font-size: 30px;
-  color: red;
-  background-color: green;
+.loader {
+  --d:22px;
+  width: 4px;
+  height: 4px;
+  margin: 300px auto 0 auto;
+  border-radius: 50%;
+  color: #25b09b;
+  box-shadow: 
+    calc(1*var(--d))      calc(0*var(--d))     0 0,
+    calc(0.707*var(--d))  calc(0.707*var(--d)) 0 1px,
+    calc(0*var(--d))      calc(1*var(--d))     0 2px,
+    calc(-0.707*var(--d)) calc(0.707*var(--d)) 0 3px,
+    calc(-1*var(--d))     calc(0*var(--d))     0 4px,
+    calc(-0.707*var(--d)) calc(-0.707*var(--d))0 5px,
+    calc(0*var(--d))      calc(-1*var(--d))    0 6px;
+  animation: loader 1s infinite steps(8);
+}
+
+@keyframes loader {
+  100% {transform: rotate(1turn)}
 }
 
 @media only screen and (min-width: 1536px) {
