5991f261370bc890ece83d2824919715a4c6c3d7
Robert L. Read <read.robert@gmail.com>
2025-03-27 16:51:50 -0500

    broken check in

--- packages/back-end/src/functions/httpFunctions.ts
+++ packages/back-end/src/functions/httpFunctions.ts
@@ -41,6 +41,7 @@ const routeFunctions: DICT_TYPE = {
   "getFile/{containerName}/{fileName}/{fileType}": getFile, // Example: "getFile/okh/bread/yml"
     "listFiles/{containerName}": listFilesByContainerName, // Example: http://localhost:7071/api/listFiles/okw OR http://localhost:7071/api/listFiles/okh
     listOKHsummaries, // This is specifically meant to provide thumbnails for the frontend
+    testMatching
 };
 //create route for each
 for (let key in routeFunctions) {
@@ -284,4 +285,209 @@ function convertToProduct(fname:string, obj: any, id: number): any | null {
     projectLink: obj["project-link"],
     manifestAuthor: obj["manifest-author"]?.name || "none",
   };
+}
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+// In this section, attempting to integrate some typescript //
+// code that Harry wrote (from Python code that Rob wrote.) //
+// Nathan imagines this moving to a different, separate API //
+// but this is a convenient place for it now.               //
+//////////////////////////////////////////////////////////////
+
+import * as YAML from 'yaml';
+
+
+
+export class Atom {
+  constructor(readonly identifier: string, readonly description?: string, readonly link?: string) {}
+
+  toString() {
+    return this.description
+      ? `${this.description} (${this.identifier})`
+      : this.identifier;
+  }
+}
+
+export function parseAtom(atom: any): Atom | undefined {
+  if (typeof atom !== "object") { return; }
+
+  const identifier = atom["identifier"] as string | undefined;
+  const description = atom["description"] as string | undefined;
+  const link = atom["link"] as string | undefined;
+
+  return identifier ? new Atom(identifier, description, link) : undefined;
+}
+
+export function parseAtomArray(atom: any) {
+  return Array.isArray(atom)
+    ? atom.map(parseAtom).filter(<T>(value: T | null | undefined): value is T => { return !!value; })
+    : [];
+}
+
+export interface OkwParty {
+  title: string;
+  supplies?: Atom[];
+  tools?: Atom[];
+  inventory?: Atom[];
+}
+
+export function parseOKW(body: string): OkwParty {
+  const yaml = YAML.parse(body);
+  const title = yaml["title"] as string | undefined;
+  if (typeof title !== "string") { throw new Error("Title must be a string"); }
+  const supplies = parseAtomArray(yaml["supply-atoms"]);
+  const tools = parseAtomArray(yaml["tool-list-atoms"])
+  const inventory = parseAtomArray(yaml["inventory-atoms"])
+  return { title, supplies, tools, inventory };
+}
+
+export interface OkhDesign {
+  title: string;
+  product: Atom;
+  bom?: Atom[];
+  tools?: Atom[];
+  bomOutput?: Atom[];
+}
+
+export function parseOKH(body: string): OkhDesign | undefined {
+  const yml = YAML.parse(body);
+  const title = yml["title"];
+  if (typeof title !== "string") { return; }
+
+  const product = parseAtom(yml["product-atom"])
+  if (!product) { return; }
+  const bom = parseAtomArray(yml["bom-atoms"])
+  const tools = parseAtomArray(yml["tool-list-atoms"])
+  const bomOutput = parseAtomArray(yml["bom-output-atoms"])
+  return { title, product, bom, tools, bomOutput };
+}
+
+export interface SupplyTree {
+  readonly product: Atom;
+  print(indent: number): void;
+}
+
+export class SuppliedSupplyTree implements SupplyTree {
+
+  constructor(readonly supplier: OkwParty, readonly product: Atom) { }
+
+  print(indent: number): void {
+    console.log(`${" ".repeat(indent)}Supplier: ${this.supplier.title}/${this.product}`);
+  }
+}
+
+export class InventorySupplyTree implements SupplyTree {
+  constructor(readonly maker: OkwParty, readonly product: Atom) { }
+
+  print(indent: number): void {
+    console.log(`${" ".repeat(indent)}Maker Inventory: ${this.maker.title}/${this.product}`);
+  }
+}
+
+export class MadeSupplyTree implements SupplyTree {
+  constructor(readonly product: Atom, readonly design: OkhDesign, readonly maker: OkwParty, readonly supplies: readonly SupplyTree[]) { }
+  print(indent: number): void {
+    console.log(`${" ".repeat(indent)}Maker: ${this.maker.title}/${this.design.title}`);
+    for (const supply of this.supplies) {
+      supply.print(indent + 2);
+    }
+  }
+}
+
+export class MissingSupplyTree implements SupplyTree {
+  constructor(readonly product: Atom) { }
+  print(indent: number): void {
+    console.log(`${" ".repeat(indent)}Missing: ${this.product}`);
+  }
+}
+
+
+export function* supplyTreeQuery(product: Atom, parties: readonly OkwParty[], designs: readonly OkhDesign[]) {
+  let found = false;
+  for (const supplier of parties) {
+    if (supplier.supplies?.some(s => s.identifier === product.identifier)) {
+      found = true;
+      yield new SuppliedSupplyTree(supplier, product);
+    }
+  }
+
+  for (const design of designs.filter(d => d.product.identifier === product.identifier)) {
+    for (const maker of parties) {
+      if (compatible(maker, design)) {
+        found = true;
+
+        const bomTrees = new Array<SupplyTree>();
+        for (const bomItem of design.bom ?? []) {
+          if (maker.inventory?.some(i => i.identifier === bomItem.identifier)) {
+            bomTrees.push(new InventorySupplyTree(maker, bomItem));
+          } else {
+            bomTrees.push(...supplyTreeQuery(bomItem, parties, designs));
+          }
+        }
+
+        yield new MadeSupplyTree(product, design, maker, bomTrees);
+      }
+    }
+
+    if (!found) {
+      yield new MissingSupplyTree(product);
+    }
+  }
+
+  function compatible({ tools: makerTools }: OkwParty, { tools: requiredTools }: OkhDesign): boolean {
+    if (!makerTools || makerTools.length === 0) { return false; }
+    if (!requiredTools || requiredTools.length === 0) { return true; }
+    return requiredTools.every(tool => makerTools.some(t => t.identifier === tool.identifier));
+  }
+}
+
+
+// This is just a test function/route.
+// We are trying to show the ability to atom matching.
+// It will have to be generalized later.
+export async function testMatching(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+
+    // const okws = new Array<OkwParty>();
+    // for await (const file of listFiles("beta/okw/")) {
+    //   if (!file.Key) { continue; }
+    //   const body = await getObject(file.Key);
+    //   const okw = body ? parseOKW(body) : undefined;
+    //   if (okw) { okws.push(okw); }
+    // }
+
+    // const okhs = new Array<OkhDesign>();
+    // for await (const file of listFiles("beta/okh/")) {
+    //   if (!file.Key) { continue; }
+    //   const body = await getObject(file.Key);
+    //   const okh = body ? parseOKH(body) : undefined;
+    //   if (okh) { okhs.push(okh); }
+    // }
+
+    const okh0 = getOKHByFileName("microscope","okh","yml");
+
+    const okhs = [okh0];
+
+    const okw0 = getOKHByFileName("microscope","okw","yml");
+    const okws = [okw0];
+    const USE_REAL_MATCHING = false;
+
+         if (USE_REAL_MATCHING) {
+             const results = supplyTreeQuery(okhs[0].product, okws, okhs);
+             for (const result of results) {
+                 result.print(0);
+
+                 // console.log(JSON.stringify(okws, undefined, 4));
+                 // console.log(JSON.stringify(okhs, undefined, 4));
+             }
+             return { jsonBody: results };
+         } else {
+             const scoredList = {"a" : "357","b" : "789"};
+             return { jsonBody: scoredList};
+         }
 }
