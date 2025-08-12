97242f1fff0c6d37d5f874f1f5b70a46afac4b38
NimeshaKahingala <nimesha.isholi94@gmail.com>
2025-08-12 17:26:12 -0500

    initial okw detail page

---
+++ packages/front-end/pages/okwdetails.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container pt-44">
+    <div>Displaying 1 to 25 out of 12345 Results</div>
+    <div class="main-title mt-8 text-2xl font-bold">
+      OKW Name / Description of OKW
+    </div>
+    <div class="description">
+      Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+      libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
+      sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur
+      neque. Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+      vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent
+      taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut
+      commodo efficitur neque.
+    </div>
+
+    <div class="filters">
+      <div>Search within</div>
+      <div>Zip</div>
+      <div>More Filter</div>
+      <div>Sort by: Most relevant ▼</div>
+    </div>
+
+    <div class="card">
+      <div class="left">img</div>
+      <div class="center">
+        <div>Product Name</div>
+        <div>Location, Country</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur. Turpis in cras nulla montes
+          accumsan diam orci venenatis lectus. Elementum interdum pellentesque
+          lobortis amet. Orci adipiscing orci in sollicitudin lorem. Aliquet
+          orci aenean velit magnis quis vel praesent nibh est. Interdum sed
+          molestie venenatis tristique. Eu eget ut a nunc tortor non enim lectus
+          viverra. Semper etiam fermentum amet id morbi.
+        </div>
+      </div>
+       <div class="right">
+        <button class="btn-primary">ORDER</button>
+        <button class="btn-secondary">SEE DETAILS</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+}
+
+.main-title {
+  color: #2a3952;
+}
+</style>
