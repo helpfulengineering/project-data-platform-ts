98a185f9528cd2d24b7ee2eb2971a476d4a30bea
NimeshaKahingala <nimesha.isholi94@gmail.com>
2025-08-14 17:15:50 -0500

    okw detail page content

--- packages/front-end/pages/okwdetails.vue
+++ packages/front-end/pages/okwdetails.vue
@@ -1,56 +1,117 @@
 <template>
-  <div class="container pt-44">
-    <div>Displaying 1 to 25 out of 12345 Results</div>
-    <div class="main-title mt-8 text-2xl font-bold">
-      OKW Name / Description of OKW
-    </div>
-    <div class="description">
-      Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
-      libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
-      sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-      Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur
-      neque. Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-      vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent
-      taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-      himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut
-      commodo efficitur neque.
-    </div>
+  <div class="container okw-detail-page">
+    <aside class="sidebar">
+      <h3>Filters</h3>
+      <ul>
+        <li><a href="#">Category 1</a></li>
+        <li><a href="#">Category 2</a></li>
+        <li><a href="#">Category 3</a></li>
+        <li><a href="#">Category 4</a></li>
+        <li><a href="#">Category 5</a></li>
+        <li><a href="#">Category 6</a></li>
+      </ul>
+      <h3>We can make</h3>
+      <ul>
+        <li><a href="#">Product 1</a></li>
+        <li><a href="#">Product 2</a></li>
+        <li><a href="#">Product 3</a></li>
+        <li><a href="#">Product 4</a></li>
+        <li><a href="#">Product 5</a></li>
+      </ul>
+      <h3>Tools and Processes</h3>
+      <ul>
+        <li><a href="#">Name</a></li>
+        <li><a href="#">Name</a></li>
+        <li><a href="#">Name</a></li>
+      </ul>
+      <h3>Roles</h3>
+      <h3>Resources</h3>
+      <h3 href="#">Availability</h3>
+      <h3>Contact Information</h3>
+    </aside>
 
-    <div class="filters">
-      <div>Search within</div>
-      <div>Zip</div>
-      <div>More Filter</div>
-      <div>Sort by: Most relevant ▼</div>
-    </div>
-
-    <div class="card">
-      <div class="left">img</div>
-      <div class="center">
-        <div>Product Name</div>
-        <div>Location, Country</div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur. Turpis in cras nulla montes
-          accumsan diam orci venenatis lectus. Elementum interdum pellentesque
-          lobortis amet. Orci adipiscing orci in sollicitudin lorem. Aliquet
-          orci aenean velit magnis quis vel praesent nibh est. Interdum sed
-          molestie venenatis tristique. Eu eget ut a nunc tortor non enim lectus
-          viverra. Semper etiam fermentum amet id morbi.
+    <main class="main-content">
+      <div class="okw-section">
+        <h2>OKW Name / Description of OKW</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          vulputate libero et velit interdum, ac aliquet odio mattis. Class
+          condimentum lobortis. Ut commodo efficitur neque. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit
+          interdum, ac aliquet odio mattis.
+        </p>
+        <div class="btn-wrap">
+          <button class="order-btn">ORDER</button>
+          <button class="contact-btn">CONTACT SUPPLIER</button>
+          <p class="contact-info">Website: <a href="#">www.example.com</a></p>
+          <p class="contact-info">Phone Number <a href="#"></a></p>
         </div>
       </div>
-       <div class="right">
-        <button class="btn-primary">ORDER</button>
-        <button class="btn-secondary">SEE DETAILS</button>
+
+      <div class="tabs">
+        <a href="#" class="active">Profile</a>
+        <a href="#">Certifications</a>
+        <a href="#">Additional Information</a>
+        <a href="#">Diversity</a>
+        <a href="#">Contact</a>
       </div>
-    </div>
+
+      <div class="tabs">
+        <a href="#">Tools</a>
+        <a href="#">Processes</a>
+        <a href="#">Products</a>
+        <a href="#">Inventory</a>
+      </div>
+
+      <div class="business-section">
+        <h2>Business Description / Name/ Entity name</h2>
+        <p>
+          Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          vulputate libero et velit interdum, ac aliquet odio mattis. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+          lobortis. Ut commodo efficitur neque. Torem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nunc vulputate libero et velit interdum,
+          ac aliquet odio mattis. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus
+          urna at turpis condimentum lobortis. Ut commodo efficitur neque.
+        </p>
+        <div class="details">
+          <div>
+            <h3>Specifications</h3>
+            <p>Detail: Lorem ipsum</p>
+            <p>Detail: Lorem ipsum</p>
+            <p>Detail: Lorem ipsum</p>
+            <p>Detail: Lorem ipsum</p>
+          </div>
+          <div>
+            <h3>Business Details</h3>
+            <p>Website: <a href="#">www.example.com</a></p>
+            <p>Product: <a href="#">Product Name</a></p>
+            <p>Torem ipsum: <a href="#">Detail</a></p>
+          </div>
+          <div>
+            <h3>Customer Reviews</h3>
+            <p>★★★★☆ 4 out of 5</p>
+            <p>Name Name - "Lorem ipsum dolor sit amet"</p>
+            <p>Name Name - "Lorem ipsum dolor sit amet"</p>
+          </div>
+          <div>
+            <h3>About Supplier</h3>
+            <p>Website: <a href="#">www.example.com</a></p>
+            <p>Product: <a href="#">Product Name</a></p>
+            <p>Torem ipsum: <a href="#">Detail</a></p>
+          </div>
+        </div>
+        <div class="verified">
+          <img src="https://via.placeholder.com/40x40?text=H" alt="Verified" />
+          <p>Helpful verified</p>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <style scoped>
-.container {
-  margin: 0 auto;
-}
 
-.main-title {
-  color: #2a3952;
-}
 </style>
